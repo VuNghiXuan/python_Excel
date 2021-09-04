c7c182336e3d735066bf702841db2412a6ed27cd
VuNghiXuan <89232420+VuNghiXuan@users.noreply.github.com>
2021-09-04 14:40:24 +0700

    Add files via upload

---
+++ sofe_exell_Panda/Package_VuNghiXuan_Exell/File_Exell.py
@@ -0,0 +1,29 @@
+import xlwings as xw
+import os
+class Workbook():
+	def __init__(self, path, name):
+		self.path = str(path)
+		self.name = str(name)
+		# self.sheets = sheets
+		self.active = False
+
+		self.pathfull = os.path.join(self.path,self.name)
+
+		print("self.pathfull------ " + self.pathfull +"\n")
+
+	def num_sheets(self):
+		num_sheet = xw.sheets
+		# count_sh = wb.api.Sheets.Count
+		# num_sheet = self.wb.api.Sheets.Count
+		print(num_sheet)
+		return num_sheet
+
+	def open(self):
+		# print("begin open(self)======    self.wb =========", self.pathfull +"\n" )
+		self.wb = xw.Book(self.pathfull)   #  = self.wb = 
+		self.active = True
+		print("thử cho print ==== ", self.wb )
+		return (self.wb )
+
+def read_file_excel()
+	
